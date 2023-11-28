a9b7bbcd96a205dcc55af4853f1bf94e51d7de72
haneen <nonakram@gmail.com>
2023-11-29 00:21:57 +0200

    i've done post and text classes but not final (4)

--- Post.java
+++ Post.java
@@ -3,38 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Post extends Text {
-<<<<<<< HEAD
-
-    private USER taggedUser;
-    // private Friendship friendship ;
-=======
     private static int Id;
     ////////////////////////////////////////////**ATTRIBUTES**//////////////////////////////////////
     private LocalDateTime timestamp;
     private List<USER> taggedUsers;
     // private Fiendship fiendship ;
->>>>>>> ab6fc5dfc4977f966a4b5912f357d4aa07d9963e
     // private List<Comment> comments;
 
-<<<<<<< HEAD
-    public Post(int id, int userId, String content, LocalDateTime timestamp, int cntReacts) {
-        super(id, userId, content, timestamp, cntReacts);
-    }
-
-    @Override
-    public void addReact(Post post, int postId) {
-
-    }
-
-    @Override
-    public int getReact() {
-        return 0;
-    }
-
-//    public Post (int Id, String username, String content){
-//        super(int id, int userId, String content, LocalDateTime timestamp, int cntReacts);
-//       // this.comments = new ArrayList<>();
-=======
     //////////////////////////////////////////**CONSTRUCTORS**///////////////////////////////////////////
     public Post() {
         super(Id++);
@@ -51,7 +26,6 @@ public class Post extends Text {
     }
 //    public void  addTaggedUser(USER taggedUsers){
 //        taggedUsers.addPost();
->>>>>>> ab6fc5dfc4977f966a4b5912f357d4aa07d9963e
 //    }
 //    public List<USER> getTaggedUsers() {
 //        return taggedUsers;
