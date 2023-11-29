4bae88a4fb307a1c9b0f82142cb5232d9a8bbf58
Renad Hossam Emara <renad03emara@gmail.com>
2023-11-29 15:40:19 +0200

    Added register method

--- USER.java
+++ USER.java
@@ -1,10 +1,34 @@
-public class USER {
-    private int UserID;
-    private String Email;
+import java.util.Scanner;
+import java.util.Vector;
+public class User
+{
+    static int UserID=0;
     private String Name;
+    private String Email;
     private String Password;
     private String Gender;
-    private String DateOfBirth;
+    private String Birthdate;
+    private int Phonenumber;
 
+    Scanner scanner = new Scanner(System.in);
 
+    private void register ()
+    {
+        System.out.println("Enter your name: ");
+        Name = scanner.nextLine();
+        System.out.println("Enter your email: ");
+        Email = scanner.nextLine();
+        System.out.println("Enter your password: ");
+        Password = scanner.nextLine();
+        System.out.println("Enter your gender: ");
+        Gender = scanner.nextLine();
+        System.out.println("Enter your phone number: ");
+        Phonenumber = scanner.nextInt();
+        System.out.println("Enter your birthdate: ");
+        Birthdate = scanner.nextLine();
+        UserID+=1;
+    }
 }
+
+
+
