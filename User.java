b3e04ed1caa5a08548339b6bfebfc33fad576ad0
Renad Hossam Emara <renad03emara@gmail.com>
2023-11-29 17:20:55 +0200

    Setters & Getters are added

--- User.java
+++ User.java
@@ -1,34 +1,77 @@
 import java.util.Scanner;
 import java.util.Vector;
+
+
 public class User
 {
-    static int UserID=0;
+    static int UserID;
     private String Name;
     private String Email;
     private String Password;
     private String Gender;
     private String Birthdate;
-    private int Phonenumber;
-
+    private int PhoneNumber;
     Scanner scanner = new Scanner(System.in);
+    Vector<String> Notifications = new Vector<>();
+    Vector<String> Friends = new Vector<>();
+    Vector<String> Posts = new Vector<>();
+    Vector<String> Conversations = new Vector<>();
 
-    private void register ()
-    {
-        System.out.println("Enter your name: ");
-        Name = scanner.nextLine();
-        System.out.println("Enter your email: ");
-        Email = scanner.nextLine();
-        System.out.println("Enter your password: ");
-        Password = scanner.nextLine();
-        System.out.println("Enter your gender: ");
-        Gender = scanner.nextLine();
-        System.out.println("Enter your phone number: ");
-        Phonenumber = scanner.nextInt();
-        System.out.println("Enter your birthdate: ");
-        Birthdate = scanner.nextLine();
-        UserID+=1;
+    public static int getUserID() {
+        return UserID;
     }
+
+    public static void setUserID(int userID) {
+        UserID = userID;
+    }
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+    public String getBirthdate() {
+        return Birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        Birthdate = birthdate;
+    }
+    public int getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+
 }
+
+
+
 
 
 
