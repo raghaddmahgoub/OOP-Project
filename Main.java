c2bdade57d93c8c227d29147000855d548d8ff04
Farah Ibrahim <71364218+farahib1@users.noreply.github.com>
2023-12-07 05:31:15 +0200

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Vector;
+
+// Press Shift twice to open the Search Everywhere dialog and type show whitespaces,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    public static ArrayList<User> vec = new ArrayList<>();
+
+    public static void main(String[] args) {
+        UserInterface.ProgramStart();
+
+
+    }
+}
